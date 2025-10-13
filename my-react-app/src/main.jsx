a93bb67f7c6ff6b63a79b87e90f2e944050e6b3a
Osekobo <collinsboseko2005@gmail.com>
2025-10-13 20:36:13 +0300

    classes

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -8,6 +8,15 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+// creating a class:
+class Car {
+  constructor(name) {
+    this.brand = name;
+  }
+}
+
+const mycar = new Car("Ford");
+document.write(mycar.brand);
 
 // cd my-react-app
 // npm run dev
@@ -15,3 +24,8 @@ createRoot(document.getElementById('root')).render(
 // install vite: npm install -g create-vite
 // to create a React application named my-react-app: npm create vite@latest my-react-app -- --template react
 // createRoot(document.getElementById('root')).render(-----)  
+
+// Arrow functions: This works only if the function has only one statement.
+// hello = function() {
+//   return "Hello World!";  =>  hello = () => "Hello World!";
+// }
