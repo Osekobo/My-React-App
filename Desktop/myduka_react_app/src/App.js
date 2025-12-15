288ad7a717b982247f84aedf75abb03cbe3e51f4
Osekobo <collinsboseko2005@gmail.com>
2025-12-15 18:06:14 +0300

    protectedroute.jsx added

--- Desktop/myduka_react_app/src/App.js
+++ Desktop/myduka_react_app/src/App.js
@@ -3,23 +3,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Index from "./pages/Index";
-// import About from "./pages/About";
+import About from "./pages/About";
 import Products from "./pages/Products";
 import Purchases from "./pages/Purchases";
 import Sales from "./pages/Sales";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Index />} />
-        {/* <Route path='/about' element={<About />} /> */}
+        <Route path='/' element={<ProtectedRoute><Index /></ProtectedRoute>} />
+        <Route path='/about' element={<ProtectedRoute><About /></ProtectedRoute>} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/products" element={<Products />} />
         <Route path="/purchases" element={<Purchases />} />
         <Route path="/sales" element={<Sales />} />
+        <Route path="/signup" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
